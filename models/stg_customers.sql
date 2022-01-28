7de9e14714ce5f198fe1198efce23ca444b24e6e
Murray Dickson <murray.dickson@iag.co.nz>
2022-01-28 00:53:08 +0000

    built dim_customer using ref to stage tables

---
+++ models/stg_customers.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized="table"
+)}}
+
+with customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from `dbt-tutorial`.jaffle_shop.customers
+
+)
+
+select * from customers order by 1
---
+++ models/stg_orders.sql
@@ -0,0 +1,17 @@
+{{ config(
+    materialized="table"
+)}}
+
+with orders as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+
+    from `dbt-tutorial`.jaffle_shop.orders
+
+)
+
+select * from orders order by 1
